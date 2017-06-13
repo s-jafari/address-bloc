17f48028504c9aad7ad87fe07741933cc0c94ca2
Shervin Jafari <shv.jafari@gmail.com>
2017-06-13 14:41:55 +0200

    remaining menu functions

--- models/address_book.rb
+++ models/address_book.rb
@@ -55,13 +55,4 @@ class AddressBook
     end
     return nil
   end
-
-  def iterative_search(name)
-    entries.each do |entry|
-      if entry.name == name
-        return entry
-      end
-    end
-    return nil
-  end
 end
